a2d59350ddd2357675c1c2c0b1f4030632538c37
bon <kaste2233@gmail.com>
2024-08-26 12:06:45 +0900

    Initialize

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,7 @@
-import React, { useEffect, useRef, useState } from 'react';
-import './App.css';
-import Canvas from './components/Canvas1';
-import Canvas2 from './components/Canvas2';
-import Canvas3 from './components/Canvas3';
+import Canvas from './components/Canvas';
 
 function App() {
-  return (
-    <div style={{ display: 'flex' }}>
-      {/* <Canvas/> */}
-      <Canvas2 />
-
-      {/* <Canvas3 /> */}
-    </div>
-  );
+  return <Canvas />;
 }
 
 export default App;
---
+++ src/components/Canvas.tsx
@@ -0,0 +1,7 @@
+const CANVAS_SIZE = 512;
+
+export default function Canvas2() {
+  const source = '/images/bottle.jpeg';
+
+  return <div id='container'></div>;
+}
